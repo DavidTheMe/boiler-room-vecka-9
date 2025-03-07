2fe70327468884caec44bf629b743829f669056a
unknown <davidweddfelt@hotmail.com>
2025-03-07 12:10:16 +0100

    no errors

--- Dashboard/app/routes/home.tsx
+++ Dashboard/app/routes/home.tsx
@@ -1,12 +1,9 @@
-import type { Route } from "./+types/home";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
 }
 
-export default function Home() {
-  return <Welcome />;
-}
+export default Home;
