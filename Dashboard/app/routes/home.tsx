8c2be1184888386f9845cf7a2ca86b3db15f3293
filipwretner <filip.wretner@chasacademy.se>
2025-03-07 11:34:44 +0100

    Enkel display utan komponenter

--- Dashboard/app/routes/home.tsx
+++ Dashboard/app/routes/home.tsx
@@ -1,12 +1,65 @@
-import type { Route } from "./+types/home";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addToPlaylist, removeFromPlaylist } from "../redux/slice";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+const Home = () => {
+  const [recentlyClicked, setRecentlyClicked] = useState<any[]>([]);
+  const [popularSongs, setPopularSongs] = useState<any[]>([]);
+  const [newReleases, setNewReleases] = useState<any[]>([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch("../data/songs.json")
+    .then((response) => response.json())
+    .then((data) => {
+      setPopularSongs(data.popularSongs);
+      setNewReleases(data.newReleases);
+    });
+  }, []);
+
+  return (
+    <div>
+      <section>
+        <h2>Heta låtar just nu</h2>
+        <div>
+          {popularSongs.map((song) => (
+            <div key={song.id}>
+              <img src={song.cover} alt={song.songTitle} />
+              <h3>{song.songTitle}</h3>
+              <p>{song.artist}</p>
+              <p>{song.albumTitle}</p>
+              <button 
+              onClick={() => dispatch(addToPlaylist(song))}
+              className = ""
+              >Lägg till i din spellista</button>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section>
+        <h2>Nya releaser</h2>
+        <div>
+          {newReleases.map((song) => (
+            <div key={song.id}>
+              <img src={song.cover} alt={song.songTitle} />
+              <h3>{song.songTitle}</h3>
+              <p>{song.artist}</p>
+              <p>{song.albumTitle}</p>
+              <button 
+              onClick={() => dispatch(addToPlaylist(song))}
+              className = ""
+              >Lägg till i din spellista</button>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section>
+
+      </section>
+    </div>
+  )
 }
 
-export default function Home() {
-  return <Welcome />;
-}
+
