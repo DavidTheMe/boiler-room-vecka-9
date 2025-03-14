cdbde313a66c969998eaf0e959b7df8013785064
unknown <davidweddfelt@hotmail.com>
2025-03-14 09:40:36 +0100

    Started adding a css

--- Dashboard/app/routes/navigator.tsx
+++ Dashboard/app/routes/navigator.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router";
+import "./navigator.css";
 
 const Navigator = () => {
   return (
     <>
+    <input></input>
     <Link to="/">Home</Link>
     <Link to="/search">Search</Link>
     <Link to="/details">Details</Link>
