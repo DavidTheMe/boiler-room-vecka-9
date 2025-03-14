f98e16d4fbfdf09acd5b6b1d905fa3aa809adec6
filipwretner <filip.wretner@chasacademy.se>
2025-03-14 09:34:49 +0100

    Re-routed global state

--- Dashboard/app/routes/homePage.tsx
+++ Dashboard/app/routes/homePage.tsx
@@ -2,20 +2,19 @@ import useFetchSongs from "../utils/FetchSongs";
 import PopularSongs from "../components/Home/ShowPopularSongs";
 import NewReleases from "../components/Home/ShowNewReleases";
 import ShowStatistics from "../components/Home/ShowStatistics";
-import Navigator from "./navigator.tsx";
-import { Provider } from "react-redux";
-import store from "../redux/store";
+import Navigator from "./navigator";
 
 const Home = () => {
   const { popularSongs, newReleases } = useFetchSongs();
 
   return (
-    // might need to change provider to router later
-    <Provider store={store}>
+
+    <Navigator>
       <PopularSongs songs={popularSongs} />
       <NewReleases songs={newReleases} />
       <ShowStatistics />
-    </Provider>
+    </Navigator>
+    
   );
 };
 
--- Dashboard/app/routes/navigator.tsx
+++ Dashboard/app/routes/navigator.tsx
@@ -1,12 +1,23 @@
 import { Link } from "react-router";
+import { Provider } from "react-redux";
+import store from "../redux/store";
+import React from "react";
+import type { ReactNode } from "react";
 
-const Navigator = () => {
+interface NavigatorProps {
+  children: ReactNode;
+}
+
+const Navigator: React.FC<NavigatorProps> = ({ children }) => {
   return (
-    <>
-    <Link to="/">Home</Link>
-    <Link to="/search">Search</Link>
-    <Link to="/details">Details</Link>
-    </>
+    <Provider store={store}>
+      <nav>
+      <Link to="/">Home</Link>
+      <Link to="/search">Search</Link>
+      <Link to="/details">Details</Link>
+      </nav>
+      <main>{children}</main>
+    </Provider>
   );
 }
 
