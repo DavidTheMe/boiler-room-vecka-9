43f0e82d45fcdef1ff73de2ba758493dc42f8f06
filipwretner <filip.wretner@chasacademy.se>
2025-03-14 10:37:11 +0100

    Implemented global state

--- Dashboard/app/routes/navigator.tsx
+++ Dashboard/app/routes/navigator.tsx
@@ -1,21 +1,30 @@
 import { Link } from "react-router";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import store from "../redux/store";
 import React from "react";
 import type { ReactNode } from "react";
+import Playlist from "../components/Global/ShowPlaylist";
 
 interface NavigatorProps {
   children: ReactNode;
 }
 
 const Navigator: React.FC<NavigatorProps> = ({ children }) => {
+
+  const songs = useSelector((state: any) => state.playlist.songs);
+
   return (
     <Provider store={store}>
-      <nav>
+      <div>
+      <section>
       <Link to="/">Home</Link>
       <Link to="/search">Search</Link>
       <Link to="/details">Details</Link>
-      </nav>
+      </section>
+      <aside>
+        <Playlist songs={songs}/>
+      </aside>
+      </div>
       <main>{children}</main>
     </Provider>
   );
